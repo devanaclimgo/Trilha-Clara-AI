f91bd3ce1099bf599c2b46985c097f1073a165eb
devanaclimgo <anaclimgo@gmail.com>
2025-08-31 11:40:49 -0300

    feat: add me action to Auth::UsersController for retrieving current user details

--- backend/app/controllers/auth/users_controller.rb
+++ backend/app/controllers/auth/users_controller.rb
@@ -1,2 +1,7 @@
 class Auth::UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def me
+    render json: { id: current_user.id, name: current_user.name, email: current_user.email }
+  end
 end
