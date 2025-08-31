e35308b072a7bcac384e8fc056e35dea17fb478a
devanaclimgo <anaclimgo@gmail.com>
2025-08-31 11:40:28 -0300

    feat: implement user registration creation in Auth::RegistrationsController with JSON response

--- backend/app/controllers/auth/registrations_controller.rb
+++ backend/app/controllers/auth/registrations_controller.rb
@@ -1,2 +1,14 @@
 class Auth::RegistrationsController < ApplicationController
+  respond_to :json
+
+  def create
+    user = User.new(params.permit(:name, :email, :password))
+    if user.save
+      sign_in(user)
+      token = request.env['warden-jwt_auth.token']
+      render json: { user: { id: user.id, name: user.name, email: user.email }, token: token }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
