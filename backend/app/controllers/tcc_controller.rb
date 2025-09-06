1aae82d9abd57a90a1c973f74f7664efdf0d5ec5
devanaclimgo <anaclimgo@gmail.com>
2025-09-05 21:02:11 -0300

    feat: added 'semanas' params for cronograma and refactored render json for better UI

--- backend/app/controllers/tcc_controller.rb
+++ backend/app/controllers/tcc_controller.rb
@@ -2,12 +2,18 @@ class TccController < ApplicationController
   def criar
     enunciado = params[:enunciado]
     curso = params[:curso]
+    semanas = params[:semanas] || 8
 
     gemini = GeminiService.new
 
-    @explicacao = gemini.simplificar_enunciado!(enunciado: enunciado, curso: curso)
-    @sumario = gemini.gerar_sumario!(curso: curso)
+    explicacao = gemini.simplificar_enunciado!(enunciado: enunciado, curso: curso)
+    sumario = gemini.gerar_sumario!(curso: curso)
+    cronograma = gemini.gerar_cronograma!(curso: curso, semanas: semanas)
 
-    render json: { explicacao: @explicacao, sumario: @sumario }
+    render json: {
+      explicacao: explicacao,
+      sumario: sumario,
+      cronograma: cronograma
+    }
   end
 end
